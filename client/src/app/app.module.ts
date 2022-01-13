da7889236c0f3a3e1ebb803c1ef2689bd9ce14c4
Jimmy-Du <jdu5780@conestogac.on.ca>
2022-01-13 02:25:34 -0500

    Updated navbar and footer imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
