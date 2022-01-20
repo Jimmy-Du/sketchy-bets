5ea7c36dc8da4f4bc194b5699818c596b477f1dc
Jimmy-Du <jdu5780@conestogac.on.ca>
2022-01-20 18:19:38 -0500

    Added auth guard to prevent users that are not logged in from accessing certain routes

---
+++ client/src/app/guards/auth/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const isAuth: boolean = this.auth.checkIfLoggedIn()
+
+    // if the user is not logged in, they are redirected to the sign in page
+    if (!isAuth) {
+      this.router.navigate(['sign-in'])
+    }
+
+    return isAuth
+  }
+}
