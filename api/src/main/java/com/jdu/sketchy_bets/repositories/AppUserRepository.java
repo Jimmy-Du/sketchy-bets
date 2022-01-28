8e5f87bf729e1872ed677516cf85d828173666dd
Jimmy-Du <jdu5780@conestogac.on.ca>
2022-01-27 20:01:00 -0500

    Added function to retrieve only balance info from user

--- api/src/main/java/com/jdu/sketchy_bets/repositories/AppUserRepository.java
+++ api/src/main/java/com/jdu/sketchy_bets/repositories/AppUserRepository.java
@@ -2,7 +2,11 @@ package com.jdu.sketchy_bets.repositories;
 import com.jdu.sketchy_bets.models.AppUser;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long>{
   AppUser findByEmail(String email);
+  @Query(value = "SELECT balance FROM \"user\" WHERE email = :email", nativeQuery = true)
+  Object getBalanceByEmail(@Param("email") String email);
 }
