cac9e5d040c0b9aea4552dc3e6bcb778108c674b
Jimmy-Du <jdu5780@conestogac.on.ca>
2022-01-27 20:02:50 -0500

    Added balance route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
+import { BalanceComponent } from './components/balance/balance.component';
 import { MatchByIdComponent } from './components/matches/match-by-id/match-by-id.component';
 import { MatchesComponent } from './components/matches/matches.component';
+import { AuthGuard } from './guards/auth/auth.guard';
 import { SignUpAndInGuard } from './guards/sign-up-and-in/sign-up-and-in.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/matches', pathMatch: 'full'},
   { path: 'matches', component: MatchesComponent },
   { path: 'matches/:matchId', component: MatchByIdComponent},
+  { path: 'balance', component: BalanceComponent, canActivate: [AuthGuard] },
   { path: 'sign-up', component: SignUpComponent, canActivate: [SignUpAndInGuard] },
   { path: 'sign-in', component: SignInComponent, canActivate: [SignUpAndInGuard] }
 ];
