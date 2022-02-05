4f64897b7bc9906f2a260a760611c5d541ac6721
Jimmy-Du <jdu5780@conestogac.on.ca>
2022-02-05 14:20:41 -0500

    Removed unused import

--- api/src/main/java/com/jdu/sketchy_bets/repositories/MatchRepository.java
+++ api/src/main/java/com/jdu/sketchy_bets/repositories/MatchRepository.java
@@ -3,7 +3,6 @@ package com.jdu.sketchy_bets.repositories;
 import java.util.List;
 
 import com.jdu.sketchy_bets.models.Match;
-import com.jdu.sketchy_bets.models.Team;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
