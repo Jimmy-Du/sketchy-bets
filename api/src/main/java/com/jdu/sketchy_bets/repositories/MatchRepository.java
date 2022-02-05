08eeab173d20d2a5a305347b861e90d363fad530
Jimmy-Du <jdu5780@conestogac.on.ca>
2022-02-05 14:11:12 -0500

    Added function to find matches where no winner has been declared

--- api/src/main/java/com/jdu/sketchy_bets/repositories/MatchRepository.java
+++ api/src/main/java/com/jdu/sketchy_bets/repositories/MatchRepository.java
@@ -1,7 +1,12 @@
 package com.jdu.sketchy_bets.repositories;
 
+import java.util.List;
+
 import com.jdu.sketchy_bets.models.Match;
+import com.jdu.sketchy_bets.models.Team;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MatchRepository extends JpaRepository<Match, Long>{}
+public interface MatchRepository extends JpaRepository<Match, Long>{
+  List<Match> findByWinnerIsNull();
+}
