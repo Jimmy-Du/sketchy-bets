c18392cbf5f5cb7e9d6ff4781fd505994bf1ed0d
Jimmy-Du <jdu5780@conestogac.on.ca>
2022-02-08 16:04:48 -0500

    Added constants related to security

---
+++ api/src/main/java/com/jdu/sketchy_bets/constants/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.jdu.sketchy_bets.constants;
+
+public class SecurityConstants {
+  public static final String SIGN_UP_URL = "/users/register";
+  public static final String KEY = "JWT SECRET KEY GOES HERE";
+  public static final String HEADER_NAME = "Authorization";
+  public static final Long EXPIRATION_TIME = 1000L*60*30;
+}
