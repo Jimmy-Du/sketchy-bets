1429b1b157309c10660077fea950a62a3cbd27bf
Jimmy-Du <du.jimmy@outlook.com>
2022-02-12 20:39:17 -0500

    Added toggleLinks function to toggle links on mobile

--- client/src/app/components/layout/navbar/navbar.component.ts
+++ client/src/app/components/layout/navbar/navbar.component.ts
@@ -16,6 +16,18 @@ export class NavbarComponent implements OnInit {
 
 
 
+  // Function:    toggleLinks()
+  // Description: called upon when the user clicks the show links button on mobile devices
+  //              and will display the navbar links
+  // Parameters:  $event: info about the event that invoked the function
+  // Return:      N/A
+  toggleLinks($event: any) {
+    const links = $event.target.nextElementSibling
+    links.classList.toggle('active')
+  }
+
+
+
   // Function:    logoutHandler()
   // Description: called upon when the user clicks the "Sign Out" button, will then
   //              attempt to log out the user
